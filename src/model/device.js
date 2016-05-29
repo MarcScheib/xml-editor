ec1ea26d2d750e59ed4e3399518c4a564e139300
Marc Scheib <marc.scheib@gmx.de>
2016-05-29 20:33:15 +0200

    feat(tag): add device properties

--- src/model/device.js
+++ src/model/device.js
@@ -5,6 +5,10 @@ import {MonitorOverride} from './monitor-override';
 export class Device extends BaseTag {
   acceptTags = [Delete, MonitorOverride];
 
+  ip;
+  auto;
+  discover;
+
   constructor() {
     super('<device>', 'Devices can be added using the <device> element. They must not be nested as this violates the tree structure. Depending on the specified attributes, a single device or multiple devices can be discovered. The devices are added to the Inventory Tree and measurements are created automatically unless specified otherwise.');
   }
@@ -12,15 +16,17 @@ export class Device extends BaseTag {
   getXML() {
     let xml = '<device';
 
-    /*xml += ' ip="' + this.ip + '"';
+    if (this.ip !== undefined) {
+      xml += ' ip="' + this.ip + '"';
+    }
 
-    if (this.auto != undefined) {
+    if (this.auto !== undefined) {
       xml += ' auto="' + this.auto + '"';
     }
 
-    if (this.discover != undefined) {
+    if (this.discover !== undefined) {
       xml += ' discover="' + this.discover + '"';
-    }*/
+    }
 
     xml += '>';
 
